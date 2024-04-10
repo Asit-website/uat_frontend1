57f3b0287931e785a17a1a8e34ae6708d8b53948
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-04-10 21:31:35 +0530

    done login screen

--- src/Components/auth/Auth.js
+++ src/Components/auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import photo from "../images/Photo.png";
+import photo from "../images/loginScreen.png";
 import kushel from "../images/kushel.png";
 import path from "../images/path.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useMain } from "../../hooks/useMain";
+
 
 const Auth = (props) => {
   const { login, setUser } = useMain();
@@ -37,8 +38,6 @@ const Auth = (props) => {
 
     let ans = await login(value);
 
-    console.log("ans",ans);
-
     if (ans.success) {
       setUser(ans.user);
       localStorage.setItem("hrms_user", JSON.stringify(ans.user));
@@ -55,7 +54,6 @@ const Auth = (props) => {
       props.setAlert("success", ans.message);
 
       if (ans.user.role === "HR") {
-        console.log(ans);
         navigate("/hrDash/profile");
       } else if (ans.user.role === "EMPLOYEE") {
         navigate("/employeeDash/update");
@@ -75,23 +73,25 @@ const Auth = (props) => {
 
       <div className="login-page">
 
-        <div className="login-page1">
-          <img className="path6" src={path} alt="path" />
-          <img className="kushel-logo" src={kushel} alt="" />
-          <img src={photo} alt="photo" />
-        </div>
-
         <div className="login-page2">
 
 
          <div className="login_new_div">
 
-          <h2>Log In </h2>
+
+          <img className="kushel-logo2" src={kushel} alt="" />
+
+
+                         <div className="singWrap">
+          <h2 className="singin">Sign in </h2>
+          <p>to access HRMS Dashboard</p>
+                         </div>
           
-          <div className="login-buttons flex items-center justify-center">
+          <div className="login-buttons flex">
             <button onClick={adminLogin} className="mr-3">Admin Login</button>
             <button onClick={userLogin} className="ml-3">User Login</button>
           </div>
+
           <div className="login-form">
             <form onSubmit={handleSubmit}>
               <div className=" flex flex-col mt-4 ">
@@ -124,15 +124,16 @@ const Auth = (props) => {
 
               <button>Log in</button>
 
-              <div className="sign-information">
-                <p>
-                Terms of Use and Privacy Policy.
-                </p>
-              </div>
+             
             </form>
           </div>
+
           </div>
           
+        </div>
+
+        <div className="login-page1">
+          <img src={photo} alt="photo" />
         </div>
 
       </div>
