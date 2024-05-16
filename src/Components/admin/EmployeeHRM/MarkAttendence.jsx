1d098faa68061421b0e4d176bf87e9fa7d3afcae
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-05-16 11:02:26 +0530

    done

--- src/Components/admin/EmployeeHRM/MarkAttendence.jsx
+++ src/Components/admin/EmployeeHRM/MarkAttendence.jsx
@@ -743,7 +743,7 @@ const MarkAttendance = ({
                         <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                           <tr>
                             <th scope="col" className="px-6 py-3 currentText">
-                              Employee  Name
+                              Employee  Name 
                             </th>
                             <th scope="col" className="px-6 py-3 currentText">
                               Department
@@ -781,7 +781,9 @@ const MarkAttendance = ({
                               </td>
                               <td className="px-6 py-4 itemANs">
                                 {
-                                  new Date(item.Date).toLocaleDateString()}
+                                  // new Date(item.Date).toLocaleDateString()
+                                  item?.Date
+                                  }
                               </td>
                               <td className="px-6 py-4 itemANs">
 
@@ -901,7 +903,7 @@ const MarkAttendance = ({
                       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                           <th scope="col" className="px-6 py-3 currentText">
-                            Employee Name
+                            Employee Name 
                           </th>
 
                           <th scope="col" className="px-6 py-3 currentText">
