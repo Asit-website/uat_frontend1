800e3ed9549d27f228db204279fd74cbc159acf8
asitmandal <asitmandal492@gmail.com>
2025-02-07 15:50:07 +0530

    this

--- src/context/MainState.js
+++ src/context/MainState.js
@@ -3,11 +3,11 @@ import MainContext from './MainContext';
 import { deleteReq, get, post, put, postDocuments } from '../Api/api'
 import { useState } from 'react';
 
-// const baseUrl = "http://localhost:5000";
+const baseUrl = "http://localhost:5000";
 // 
 // const baseUrl = "https://hrms-backend-code.onrender.com"
 
-const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000";
 
 // const baseUrl = "https://hrms-backend-q2ta.onrender.com";
 
